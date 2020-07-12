ea2514275cb9b911dfd003755bbe80c4265c37bf
Stephen Nicholas <842691+stephendnicholas@users.noreply.github.com>
2020-07-12 17:51:50 +0100

    Day three GMTK GameJam

--- TetrizoGame/Assets/Scripts/Firework.cs
+++ TetrizoGame/Assets/Scripts/Firework.cs
@@ -14,26 +14,28 @@ public class Firework : MonoBehaviour {
 
     private IEnumerator Fade() {
 
-        float randoInt = Random.Range(0.0f, 5.0f);
-
-        yield return new WaitForSeconds(randoInt);
-
         while (true) {
 
             Color currentColor = fireworkImage.color;
 
-            if(currentColor.a <= 0) {
+            if(currentColor.a <= 0.1f) {
+                float pauseFor = Random.Range(0.0f, 2f);
+
+                yield return new WaitForSeconds(pauseFor);
+
                 //Time to bloom
                 currentColor.a = 1.0f;
+
+                yield return new WaitForSeconds(1f);
             }
             else {
                 // Reduce it
-                currentColor.a -= 0.1f;
+                currentColor.a -= 0.04f;
             }
 
             fireworkImage.color = currentColor;
 
-            yield return new WaitForSeconds(.5f);
+            yield return new WaitForSeconds(.1f);
         }
     }
 }
