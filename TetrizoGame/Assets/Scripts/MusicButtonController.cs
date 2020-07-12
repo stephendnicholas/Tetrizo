c386bc9b9dcb740430731d7d1c001f467eeae7ee
Stephen Nicholas <842691+stephendnicholas@users.noreply.github.com>
2020-07-12 12:34:19 +0100

    Day three GMTK GameJam

--- TetrizoGame/Assets/Scripts/MusicButtonController.cs
+++ TetrizoGame/Assets/Scripts/MusicButtonController.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.UI;
 
--- TetrizoGame/Assets/Scripts/PauseManager.cs
+++ TetrizoGame/Assets/Scripts/PauseManager.cs
@@ -12,15 +12,31 @@ public class PauseManager : MonoBehaviour
             print("Esc pressed");
 
             if (GameManager.Instance.isPaused()) {
-                pauseUI.SetActive(false);
-                GameManager.Instance.setPaused(false);
-                Time.timeScale = 1f;
+                unpause();
             }
             else {
-                pauseUI.SetActive(true);
-                GameManager.Instance.setPaused(true);
-                Time.timeScale = 0f;
+                pause();
             }
         }
+    }
+
+
+    private void pause() {
+        pauseUI.SetActive(true);
+        GameManager.Instance.setPaused(true);
+        Time.timeScale = 0f;
+    }
+
+
+    private void unpause() {
+        pauseUI.SetActive(false);
+        GameManager.Instance.setPaused(false);
+        Time.timeScale = 1f;
+    }
+
+
+    public void quitToMainMenu() {
+        unpause();
+        GameManager.Instance.MainMenu();
     }
 }
